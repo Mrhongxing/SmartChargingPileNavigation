24846c32b51cb7ad87ba383f63d0b2f1f8c1ef7b
Mrhongxing <154480715+Mrhongxing@users.noreply.github.com>
2026-05-30 23:51:23 +0800

    Update SecurityConfig.java

--- charging/src/main/java/cyou/tianshu/charging/config/SecurityConfig.java
+++ charging/src/main/java/cyou/tianshu/charging/config/SecurityConfig.java
@@ -59,7 +59,8 @@ public class SecurityConfig {
             "http://localhost:8080",  // Vue开发服务器或后端地址
             "http://localhost:5173",  // Vite开发服务器
             "http://localhost:3000",   // 其他可能的端口
-            "https://charging.tianshu.cyou"     // 生产环境可能的地址
+            "https://charging.tianshu.cyou" ,    // 生产环境可能的地址
+            "https://charging.hong.email" 
         ));
         
         // 允许的HTTP方法
